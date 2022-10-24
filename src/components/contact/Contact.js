bfb703fdad0f1ba1d0236e8303f1b22321c3d82b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-10-24 23:42:14 +0200

    Fjern ubrukt import

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import "./Contact.css";
 import emailjs from "@emailjs/browser";
 import { MdOutlineEmail } from "react-icons/md";
