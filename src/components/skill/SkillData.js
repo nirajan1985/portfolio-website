8b38050b22201d715136e4d984dbaad40173f65c
Nirajan Karki <nirajankarki1985@gmail.com>
2023-06-19 22:39:38 +0200

    Update

--- src/components/skill/SkillData.js
+++ src/components/skill/SkillData.js
@@ -3,8 +3,6 @@ import css from "../../img/css.png";
 import js from "../../img/js.png";
 import react from "../../img/react.png";
 import ts from "../../img/ts.png";
-import figma from "../../img/figma.png";
-import tailwind from "../../img/tailwind.png";
 
 const SkillList = [
   {
@@ -26,14 +24,6 @@ const SkillList = [
   {
     skillName: "React",
     logo: react,
-  },
-  {
-    skillName: "Figma",
-    logo: figma,
-  },
-  {
-    skillName: "Tailwind",
-    logo: tailwind,
   },
 ];
 export default SkillList;
